81a74cdb66e6f33652f004e69ef3f8777dd879b1
kot8585 <ansgywjd321@gmail.com>
2023-03-12 00:03:51 +0900

    prism 사용해서 code 태그 스타일링

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import { GlobalStyle } from "../styles/global-styles";
 import { DarkModeProvider } from "../context/DarkModeContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "../styles/prism-duotone-sea.css";
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
