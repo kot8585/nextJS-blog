cf40f284e5c23fc2de1fd95a94cdd8c76ed0a199
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2023-04-27 10:34:32 +0900

    search컴포넌트 debounce 처리

--- components/organisms/PostSearch.tsx
+++ components/organisms/PostSearch.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { GoSearch } from "react-icons/go";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useCallback, useRef, useState } from "react";
 import { PostData } from "../../service/post";
 import PostsGrid from "./PostsGrid";
 
@@ -11,17 +11,33 @@ type Props = {
 
 export default function PostSearch({ posts }: Props) {
   const [text, setText] = useState<string>("");
+  const [searchedPosts, setSearchedPosts] = useState<PostData[] | undefined>(
+    undefined
+  );
+  const timeoutId = useRef<number | null>(null);
 
-  let filterPostData;
-  if (text) {
-    filterPostData = posts.filter((post) =>
+  const searchByText = (text: string) => {
+    if (!text) {
+      setSearchedPosts(undefined);
+      return;
+    }
+    const searchPosts = posts.filter((post) =>
       post.content.toLowerCase().includes(text)
     );
-  }
+    setSearchedPosts(searchPosts);
+  };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => 
+  const debounceSearch = (callback: (text: string) => void, delay: number) => {
+    if (timeoutId.current) {
+      window.clearTimeout(timeoutId.current);
+    }
+    timeoutId.current = window.setTimeout(callback, delay);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
-  
+    debounceSearch(() => searchByText(e.target.value), 500);
+  };
 
   return (
     <section className="w-5/6 flex-col mx-auto  mt-20">
@@ -36,12 +52,13 @@ export default function PostSearch({ posts }: Props) {
         <GoSearch className="absolute left-3 top-4 text-2xl" />
       </div>
       <p className="py-5">
-        {filterPostData &&
-          (filterPostData.length > 0
-            ? `총 ${filterPostData.length}개의 포스트를 찾았습니다.`
+        {text &&
+          searchedPosts &&
+          (searchedPosts.length > 0
+            ? `총 ${searchedPosts.length}개의 포스트를 찾았습니다.`
             : "검색 결과가 없습니다.")}
       </p>
-       {filterPostData && <PostsGrid posts={filterPostData} />}
+      {searchedPosts && <PostsGrid posts={searchedPosts} />}
     </section>
   );
 }
